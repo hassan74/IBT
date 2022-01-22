824e47037937f47a5d283258d49a857d1c9fe11f
m.hassan@sprints.ws <sprintsws_123123>
2022-01-22 23:33:56 +0200

    Add more unit tests

--- app/src/main/java/com/unifi/ibt/data/ui/WordsUtils.kt
+++ app/src/main/java/com/unifi/ibt/data/ui/WordsUtils.kt
@@ -2,25 +2,25 @@ package com.unifi.ibt.data.ui
 
 import com.unifi.ibt.data.models.Word
 
-internal fun parseHTML(response: String): ArrayList<Word> {
+internal fun parseHTML(response: String?): ArrayList<Word> {
     val parsedHtml = ArrayList<Word>()
     response
         //Get text inside body TAG
-        .substringAfter("<body>")
-        .substringBeforeLast("</body>")
+        ?.substringAfter("<body>")
+        ?.substringBeforeLast("</body>")
         //Remove Script TAGS
-        .replace("<script (.|\n)*?</script>".toRegex(), " ")
+        ?.replace("<script (.|\n)*?</script>".toRegex(), " ")
         //Remove ALL TAGS
-        .replace("<(.|\n)*?>|</(.|\n)*?>".toRegex(), " ")
+        ?.replace("<(.|\n)*?>|</(.|\n)*?>".toRegex(), " ")
         //Remove special chars
-        .replace("[^a-zA-Z1-9]".toRegex(), " ")
-        .trim()
+        ?.replace("[^a-zA-Z1-9]".toRegex(), " ")
+        ?.trim()
         //Chars with a space before/after it.
-        .split(" ")
-        .filter { it.isNotEmpty() }
-        .groupingBy { it }
-        .eachCount()
-        .map {
+        ?.split(" ")
+        ?.filter { it.isNotEmpty() }
+        ?.groupingBy { it }
+        ?.eachCount()
+        ?.map {
             parsedHtml.add(Word(it.key, it.value))
         }
     return parsedHtml
--- app/src/test/java/com/unifi/ibt/data/ui/WordsUtilsTest.kt
+++ app/src/test/java/com/unifi/ibt/data/ui/WordsUtilsTest.kt
@@ -1,5 +1,6 @@
 package com.unifi.ibt.data.ui
 
+import com.unifi.ibt.data.models.Word
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -7,10 +8,35 @@ import org.junit.Assert.*
 class WordsUtilsTest {
 
     @Test
-    fun parsHTML_Return20() {
+    fun parsHTML_Returns20() {
         //Color text and another color, and now back to the same. Oh, and here's a different background color just in case you need it!
-        var htmlTest1 ="<p style=\"color:#B22222\">Color text and <span style=\"color:limegreen;\">another color</span>, and now back to the same. Oh, and here's a <span style=\"background-color:PaleGreen;\">different background color</span> just in case you need it!</p"
+        val htmlTest1 ="<p style=\"color:#B22222\">Color text and <span style=\"color:limegreen;\">another color</span>, and now back to the same. Oh, and here's a <span style=\"background-color:PaleGreen;\">different background color</span> just in case you need it!</p>"
         val result =parseHTML(htmlTest1)
         assertEquals(result.size, 20 )
+        assert(result.contains(Word("and",3)))
+        assert(result.contains(Word("color",3)))
+    }
+    @Test
+    fun parsHTML_empty_Returns0() {
+        //Color text and another color, and now back to the same. Oh, and here's a different background color just in case you need it!
+        val htmlTest2 ="<p style=\"color:" +
+                "#B22222\">" +
+                "<span style=\"color:limegreen;\"> " +
+                "</span>, <span style=\"background-color:PaleGreen;\"></span> !</p>"
+        val result =parseHTML(htmlTest2)
+        assertEquals(result.size, 0 )
+        assert(result.isEmpty())
+    }
+    @Test
+    fun parsHTML_null_Returns0() {
+        val htmlTest3 =null
+        val result =parseHTML(htmlTest3)
+        assertEquals(result.size, 0 )
+    }
+    @Test
+    fun parsHTML_one_Returns1() {
+        val htmlTest4 ="one One ONE"
+        val result =parseHTML(htmlTest4)
+        assertEquals(result.size, 1 )
     }
 }
